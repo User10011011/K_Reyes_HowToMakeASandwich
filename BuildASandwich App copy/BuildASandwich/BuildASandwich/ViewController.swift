c58df014f067a5b7be7188120d5dc372fdc532c6
User10011011 <67116691+User10011011@users.noreply.github.com>
2020-08-03 18:50:07 -0500

    The assignment thing
    So, basically, have a good day!

---
+++ BuildASandwich App copy/BuildASandwich/BuildASandwich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuildASandwich
+//
+//  Created by student on 8/3/20.
+//  Copyright © 2020 Kevin Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+
+
+
+
+
